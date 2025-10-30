f48415d67d4092f8e65ee5ef1bafa6bdde2bb9c8
elbse <changpriego@gmail.com>
2025-10-30 10:55:56 +0800

    Lesson03

---
+++ functions.py
@@ -0,0 +1,29 @@
+# Parameters and Arguments
+
+# name = input("Enter your name:")
+
+# def greet(name):
+#     print(f"Hello, {name}!")
+
+# greet(name)
+
+#  Return Values
+
+# def add(a,b):
+#     return a + b
+
+# result = add(5, 3)
+# print({result})
+
+# Default Parameters
+
+# def greet(name="friend"):
+#     print(f"Hello, {name}!")
+
+# greet()
+# greet("Naomie")
+
+def describe_person(name, age):
+    print(f"Hi {name}, you are {age} years old.")
+
+describe_person("Alice", 30)
