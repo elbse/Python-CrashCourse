37a605c15b1f5c468c56b57de859ba8ea2ec2bdd
elbse <changpriego@gmail.com>
2025-10-31 11:43:18 +0800

    more on loops and concept of argument and parameter

--- conditional.py
+++ conditional.py
@@ -18,11 +18,15 @@
 
 # For else 
 
-successful = False
-for number in range(1, 4):
-    print("Attempt")
-    if successful:
-        print("Successful")
-        break
-else:
-    print("Attempted 3 times and failed")
+# successful = False
+# for number in range(1, 4):
+#     print("Attempt")
+#     if successful:
+#         print("Successful")
+#         break
+# else:
+#     print("Attempted 3 times and failed")
+
+# for x in range(5):
+#     for y in range(3):
+#         print(f"({x}, {y})")
