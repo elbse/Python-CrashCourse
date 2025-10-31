60bfc28715f5109e3472a640caab54e89ade5e3f
elbse <changpriego@gmail.com>
2025-10-31 13:53:17 +0800

    Adding students

---
+++ oop.py
@@ -0,0 +1,51 @@
+# class Dog: 
+
+#     def meow(self):
+#         print("Meow!")
+
+#     def bark(self):
+#         print("Woof!")
+
+# d = Dog()
+# print(type(d))4
+
+class Student:
+    def __init__(self, name, age, grade):
+     self.name = name
+     self.age = age
+     self.grade = grade 
+
+    def get_grade(self):
+        return self.grade
+     
+class Course:
+    def __init__ (self, name, max_students):
+       self.name = name
+       self.max_students = max_students
+       self.students = []
+
+    def add_student(self, student):
+       if len(self.students) < self.max_students:
+          self.students.append(student)
+          return True
+       return False
+    
+    def get_average_grade(self):
+       value = 0
+       for student in self.students:
+          value += student.get_grade()
+
+       return value / len(self.students)
+
+s1 = Student("John", 20, 89)
+s2 = Student("Jane", 22, 92)
+s3 = Student("Dave", 21, 90)
+
+
+course = Course("History", 2)
+course.add_student(s1)
+course.add_student(s2)
+print(course.students)
+print(course.get_average_grade())
+
+
