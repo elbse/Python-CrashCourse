de718b6de9c4eb109b79355b40ffa626574697d8
elbse <changpriego@gmail.com>
2025-10-31 14:00:14 +0800

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,30 @@
+class Pet:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"{self.name} is {self.age} years old.")
+
+    def speak(self):
+        print("I don't know what to say.")
+
+class Dog(Pet):
+    def speak(self):
+        print("Woof")
+
+class Cat(Pet):
+    def speak(self):
+        print("Meow")
+
+
+p = Pet("Buddy", 4)
+p.show()
+
+c = Cat("Whiskers", 3)
+c.show()
+c.speak()
+
+d = Dog("Rex", 5)
+d.show()
+d.speak()
